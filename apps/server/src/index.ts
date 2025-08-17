4160c505bd64802b7fd4a94536afb5ac258f8209
saritakarwaa <karwasarita672@gmail.com>
2025-08-17 18:06:29 +0530

    done

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,15 @@
 import http from "http"
-
+import SocketService from './services/socket.js'
+import { startMessageConsumer } from "./services/kafka.js"
 async function init() {
+    startMessageConsumer()
+    const socketService=new SocketService()
     const httpServer=http.createServer()
     const PORT=process.env.PORT ? process.env.PORT : 8000
+    socketService.io.attach(httpServer)
     httpServer.listen(PORT,()=>{
         console.log(`HTTP server start at port:${PORT}`)
     })
+    socketService.initListeners()
 }
 init()
